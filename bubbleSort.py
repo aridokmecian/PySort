d8bcac1a7e2c3d642683688b3fae062fb3bb76e2
Ari Dokmecian <aridokmecian@gmail.com>
2019-01-28 00:36:49 -0500

    order

--- bubbleSort.py
+++
@@ -1,9 +0,0 @@
-def bubbleSort(arr):
-    #Iterate backwards through elements from index at len(arr) to 0
-    for iteration in range (len(arr) - 1, 0, -1):
-        #iterates through elements up to specificed iteration in previous line
-        for i in range(iteration):
-            #swaps elements if the current element is larger then the following element
-            if arr[i] > arr[i + 1]:
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-    return arr
--- sortTester.py
+++
@@ -1,6 +0,0 @@
-from bubbleSort import bubbleSort
-
-arr = [4,3,22,3,431,1]
-print(arr)
-print(bubbleSort(arr))
-print(arr)
