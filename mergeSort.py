d9542143e6337deed8e8be644b7d5ca48ecc7319
Ari Dokmecian <aridokmecian@gmail.com>
2019-01-28 01:11:44 -0500

    began MergeSort

---
+++ mergeSort.py
@@ -0,0 +1,36 @@
+def mergeSort(arr):
+        print("hello")
+
+def merge(dest, arrL, arrR):
+        print("hello")
+
+
+#     if len(arr) > 1:
+#         m = len(arr) / 2
+#         left = arr[0:m]
+#         right = arr[m:arr(len)]
+
+#         mergeSort(left)
+#         mergeSort(right)
+
+        
+
+
+        
+
+#     mergeSortHelper(arr, 0, len(arr))
+
+# def mergeSortHelper(arr, l, r):
+#     if r > l:
+#         m = (l + r) / 2
+#         mergeSortHelper(arr, l , m)
+#         mergeSortHelper(arr, m + 1, r)
+#         merge(arr, mergeSortHelper(arr, l , m), mergeSortHelper(arr, m + 1, r))
+
+# def merge(arr, l, r):
+#     arr = l + r
+
+
+# arr = [1,4,4,445,4]
+# mergeSort(arr)
+# print(arr)
